639815bdca48f7185d2b56a9eebda79893bb74ea
Charles Feduke <charles.feduke@gmail.com>
2012-01-25 08:35:44 -0500

    more work on the generator

--- lib/generators/active_avro/schema_dump/schema_dump_generator.rb
+++ lib/generators/active_avro/schema_dump/schema_dump_generator.rb
@@ -4,6 +4,8 @@ module ActiveAvro
   module Generators
     class SchemaDumpGenerator < Rails::Generators::NamedBase
       argument :name, :type => :string
+      class_option :human_readable, :type => :boolean, :default => true, :alias => '-h', :desc => 'Outputs Avro schema file in human readable JSON (instead of compact)'
+      class_option :console, :type => :boolean, :default => true, :alias => '-c', :desc => 'Output to STDOUT instead of file.'
 
       IGNORE_FILTER_PATH = 'config/active_avro_ignore_filter.yml'
 
@@ -14,15 +16,31 @@ module ActiveAvro
           begin
             filter = ActiveAvro::Filter.from_yaml(File.open(IGNORE_FILTER_PATH))
           rescue
-            puts %Q{Warning: unable to YAML::load #{IGNORE_FILTER_PATH} due to a problem.}
+            console_message %Q{Warning: unable to YAML::load #{IGNORE_FILTER_PATH} due to a problem.}
           end
         else
           filter = ActiveAvro::Filter.new
-          puts %Q{Warning: no "#{IGNORE_FILTER_PATH}" file, all attributes will be reflected in the schema.}
+          console_message %Q{Warning: no "#{IGNORE_FILTER_PATH}" file, all attributes will be reflected in the schema.}
         end
 
         s = ActiveAvro::Schema.new(name.constantize, filter: filter)
-        puts s.to_json
+        str_json = s.to_json
+        if options[:human_readable]
+          begin
+            json = JSON.parse(str_json)
+            str_json = JSON.pretty_unparse(json)
+          rescue
+            console_message "Error: #{$!}"
+          end
+        end
+        if options[:console]
+          puts str_json
+        end
+      end
+
+      private
+      def console_message(msg)
+        puts msg unless options[:quiet]
       end
     end
   end
