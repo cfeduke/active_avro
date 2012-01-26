950c41697b388876ff8b6a9dcfe6c8ffe63368ea
Charles Feduke <charles.feduke@gmail.com>
2012-01-26 14:21:13 -0500

    refactor complete

--- lib/active_avro/complex/record.rb
+++ lib/active_avro/complex/record.rb
@@ -78,25 +78,6 @@ module ActiveAvro
         h
       end
 
-      class Field
-        attr_accessor :name, :type
-        def initialize(name, type)
-          @name, @type = name, type
-        end
-
-        def self.from_column(column)
-          Field.new(column.name, TypeConverter.to_avro(column.type))
-        end
-
-        def to_partial_schema
-          type =
-              case
-                when @type.is_a?(Symbol) then @type.to_s
-                else @type.to_partial_schema
-              end
-          { :name => @name, :type => type }
-        end
-      end
     end
   end
 end
--- spec/active_avro/complex/record_spec.rb
+++ spec/active_avro/complex/record_spec.rb
@@ -46,36 +46,6 @@ module ActiveAvro
         end
       end
 
-      describe Record::Field do
-        describe "self#from_column" do
-          subject { Record::Field.from_column(Person.columns.find { |c| c.name == 'name' }) }
-          its("name") { should == 'name' }
-          its("type") { should == :string }
-        end
-        describe "#to_hash" do
-          subject { Record::Field.new('xyz', TypeConverter.to_avro(:integer)).to_partial_schema }
-          its([:name]) { should == 'xyz' }
-          context "when type is a symbol" do
-            its([:type]) { should == TypeConverter.to_avro(:integer).to_s }
-          end
-          context "when type is a record" do
-            subject { Record::Field.new('xyz', Record.new(Pet)).to_partial_schema }
-            its([:type]){ should be_a Hash }
-          end
-          context "when type is an enum" do
-            subject { Record::Field.new('gender', Enum.new(Gender)).to_partial_schema }
-            its([:type]){ should be_a Hash }
-            its([:name]){ should == 'gender' }
-            it "should have the expected enumerated values" do
-              e = subject[:type]
-              e[:type].should == 'enum'
-              e[:name].should == 'Gender'
-              e[:symbols].should == %w((All) Male Female)
-            end
-          end
-        end
-      end
-
     end
   end
 end
