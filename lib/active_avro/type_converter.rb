146964b8e612c1d043364ddcfee39752613f246e
Charles Feduke <charles.feduke@gmail.com>
2012-01-27 11:06:17 -0500

    TypeConverter now converts registered types

--- lib/active_avro/type_converter.rb
+++ lib/active_avro/type_converter.rb
@@ -1,5 +1,6 @@
 module ActiveAvro
   module TypeConverter
+    @@registered_converters = { }
     PRIMITIVES = {
         :primary_key => :int, # this is possibly incorrect
         :string => :string,
@@ -18,5 +19,32 @@ module ActiveAvro
     def TypeConverter.to_avro(sym)
       PRIMITIVES[sym]
     end
+
+    # register a proc for the conversion of type values
+    def TypeConverter.register(ruby_type, proc)
+      @@registered_converters[ruby_type] = proc
+    end
+
+    def TypeConverter.is_registered?(ruby_type)
+      @@registered_converters.has_key? ruby_type
+    end
+
+    # clears the type conversion registrations and then re-registers the default converters
+    def TypeConverter.reset_registrations
+      @@registered_converters.clear
+      TypeConverter.register_default_converters
+    end
+
+    def TypeConverter.convert(value)
+      return TypeConverter.is_registered?(value.class) ? @@registered_converters[value.class].call(value) : value
+    end
+
+    def TypeConverter.register_default_converters
+      # Avro doesn't support date/time or time types so we convert those to
+      # milliseconds since epoch by default
+      TypeConverter.register(Time, Proc.new { |t| t.to_i * 1_000 })
+    end
+
+    TypeConverter.register_default_converters
   end
 end
--- spec/active_avro/type_converter_spec.rb
+++ spec/active_avro/type_converter_spec.rb
@@ -18,5 +18,60 @@ module ActiveAvro
         f.call(:boolean).should == :boolean
       end
     end
+
+    describe "#is_registered? and #register" do
+      context "when no types are registered" do
+        subject { TypeConverter.is_registered?(String) }
+        it { should be_false }
+      end
+      context "when a type is registered" do
+        before { TypeConverter.register(String, Proc.new {|t| t}) }
+        after { TypeConverter.reset_registrations }
+        subject { TypeConverter.is_registered?(String) }
+        it { should be_true }
+      end
+    end
+
+    describe "#convert" do
+      context "when no types are registered" do
+        subject { TypeConverter.convert(12) }
+        it { should == 12 }
+      end
+      context "when a converter is registered" do
+        before { TypeConverter.register(Fixnum, Proc.new { |i| i * 2 }) }
+        after { TypeConverter.reset_registrations }
+        subject { TypeConverter.convert(12) }
+        it { should == 24 }
+      end
+    end
+
+    describe "#reset_registrations" do
+      context "when registering String and then resetting" do
+        before do
+          TypeConverter.register(String, Proc.new{ |s| s })
+          TypeConverter.reset_registrations
+        end
+        it "should have Time as a registered converter" do
+          TypeConverter.is_registered?(Time).should be_true
+        end
+        it "should not have String as a registered converter" do
+          TypeConverter.is_registered?(String).should be_false
+        end
+      end
+    end
+
+    describe "#self" do
+      context "when initialized" do
+        it "should have Time as a registered converter" do
+          TypeConverter.is_registered?(Time).should be_true
+        end
+        describe "Time converter" do
+          it "should convert time to the expected milliseconds since epoch value" do
+            t = Time.parse('2012-01-27 10:56:17')
+            TypeConverter.convert(t).should == 1327679777000
+          end
+        end
+      end
+    end
   end
 end
